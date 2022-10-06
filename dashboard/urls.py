6de9e69d97a60cea71c1a6ee35a8571f5d3bc4a6
naoki0729 <clair.de.lune.19957@gmail.com>
2022-10-06 18:26:05 +0900

    2,3P add

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = 'dashboard'
 urlpatterns = [
 #    path('', views.MainView.dashboard_view, name='main'),
-   path('', views.MainView.datatable_view, name='main'),
+  path('', views.MainView.datatable_view, name='main'),
 #    path('', views.MainView.myapp_list_view, name='main'),
+  path('dashboard2/', views.Dash2_view, name='dashboard2'),
+  path('dashboard3/', views.Dash3_view, name='dashboard3'),
+
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 
-template_name = "dashboard/main.html"
+
 
 class MainView:
-  
+    
+    # 棒グラフを出したい時
     # def dashboard_view(request): # ViewFunctionと呼ぶ
     #    chart_label = ['レッド', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
     #    chart_data = [1, 5, 1, 0, 0, 0]
     #    context = {'chart_data': chart_data, 'chart_label': chart_label}
     #    return render(request,template_name,context)
     
-    
+    # datatableを出したい時
     def datatable_view(request): # ViewFunctionと呼ぶ
+        template_name = "main.html"
         ctx = {}
         ctx["object_list"] = [{'id':1, 'name':'北海道'},{'id':2, 'name':'青森県'},{'id':3, 'name':'岩手県'}]
         return render(request, template_name, ctx)
 
+    # リストが機能しているか見たい時
     # def myapp_list_view(request):
     #     ctx = {}
     #     ctx["object_list"] = ["python", "javascript", "html", "css"]
@@ -25,3 +28,19 @@ class MainView:
     #     ctx["object_dict"] = {"P": "python", "J": "javascript", "H": "html", "C": "css"}
     #     ctx["object_empty"] = []
     #     return render(request, template_name, ctx)
+
+# class Dash2View(ListView):
+#     template_name = "dashboard2.html"
+
+# class Dash3View(ListView):
+#     template_name = "dashboard3.html"
+
+def Dash2_view(request):
+    chart_label = ['レッド', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+    chart_data = [1, 5, 1, 0, 0, 0]
+    context = {'chart_data': chart_data, 'chart_label': chart_label}
+    return render(request, "dashboard2.html",context)
+
+def Dash3_view(request):
+    
+    return render(request, "dashboard3.html")
